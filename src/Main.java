0d2299ffa91ace838e4d2f5fe3ebf8a48b417a7c
nurzat <nurzattalajbekova85@gmail.com>
2022-11-09 02:49:27 +0600

    щщ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
+    public  static int years;
+    public static int standUp;
+    public static boolean myOldFriends = false;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double[] maney = {0.4,47,5.5,-88,55.48,8.8,7,5.4,7,794.7,22,12.2,33.3,8.-88,-4,};
+        for (double number :maney) {
+            if (number < 0) {
+                myOldFriends= true;
+
+            }
+            if (myOldFriends){
+                if(number > 0) {
+                    years++;
+                    standUp += number;
+
+                }
+            }
+        }
+        System.out.println(standUp/ years);
     }
 }
